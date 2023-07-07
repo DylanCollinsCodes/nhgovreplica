ea1c4734401df8f39734d10b25bd550a537c9e74
Dylan Collins <biodcollins@gmail.com>
2023-07-07 17:21:38 -0400

    new javascript font-size changes

--- js/textsizer2.js
+++ js/textsizer2.js
@@ -1,7 +1,28 @@
 // JavaScript Document
 
-/* 
-** -----------------------------------------
+const max = 6
+const min = 0
+
+var size = 0
+
+const adjustFontSize = (num) => {
+	if((size < max && num == 1) || (size > min && num == -1)){
+		var allElements = document.body.getElementsByTagName('*');
+
+		for (var i = 0; i < allElements.length; i++) {
+		  var element = allElements[i];
+		  var currentFontSize = window.getComputedStyle(element).fontSize;
+		  var currentFontSizeValue = parseFloat(currentFontSize);
+		  var newFontSize = currentFontSizeValue + num;
+		  element.style.fontSize = `${newFontSize}px`;
+		}
+		size += num
+		localStorage.setItem('fontSize', size)
+	}
+}
+
+// Everything below this is legacy code
+/*-----
 ** text sizer State of NH, Web Services Division
 ** created: 02 Nov 2010
 ** ----------------------------------------- 
